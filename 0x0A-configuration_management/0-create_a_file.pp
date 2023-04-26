2b050beb0a48a72659fc6696303b465421f68d10
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2023-04-26 08:33:29 -0700

    task 2, 3

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,6 +4,6 @@ file { '/tmp/school':
   ensure  => present,
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => 0744,
+  mode    => '0744',
   content => 'I love Puppet',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # apuppet script to kill killnenow process
 
 exec { 'pkill -9 killmenow':
-  path  => 'usr/bin',
+  path  => '/usr/bin',
 }
